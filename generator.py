bde0c4ffdf04505b9d22133c9ee7e23a38acdf56
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-03-14 20:07:36 -0300

    feat: generator.py This script generates a new docker-compose config file

---
+++ generator.py
@@ -0,0 +1,69 @@
+from sys import argv
+
+SERVER_SERVICE = """
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+
+CLIENT_SERVICE = """
+  client-service-name:
+    container_name: client-container-name
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=1
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+
+NETWORKS = """
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+
+def save(file: str, data: str) -> None:
+    with open(file, "w") as f:
+        f.write(data)
+
+
+def run(file: str, n_clients: int) -> None:
+    data: str = "name: tp0\nservices:\n" + SERVER_SERVICE
+
+    for i in range(n_clients):
+        data += CLIENT_SERVICE \
+            .replace("client-service-name", f"client{i+1}") \
+            .replace("client-container-name", f"client{i+1}")
+
+    data += NETWORKS
+
+    save(file, data)
+
+
+def main() -> None:
+    if len(argv) < 3:
+        print("Expected: python3 generator.py <file> <n_clients>")
+        exit(1)
+
+    file: str = argv[1]
+    n_clients: int = int(argv[2])
+
+    run(file, n_clients)
+
+
+if __name__ == "__main__":
+    main()
