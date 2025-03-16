e61fb256f8b4851726a03798cf761b1b6403c98c
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-03-16 18:13:18 -0300

    feat: BetPacket struct BetPacket struct that implement Packet interface

---
+++ client/comms/packets/bet.go
@@ -0,0 +1,68 @@
+package packets
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// BetPacket Struct that encapsulates the bet packet data
+type BetPacket struct {
+	Agency    int
+	FirstName string
+	LastName  string
+	Document  string
+	Birthdate string
+	Number    int
+}
+
+// Error returned when unknown fields for a BetPacket are received
+type deserializationError struct {
+	Data string
+}
+
+func (e *deserializationError) Error() string {
+	return "Deserialization error, expected <document> <bet-number>, got: " + e.Data
+}
+
+// NewBetResponse Deserializes a message into a BetPacket struct
+func NewBetResponse(data string) (*BetPacket, error) {
+	split := strings.Split(data, " ")
+
+	if len(split) != 2 {
+		return nil, &deserializationError{Data: data}
+	}
+
+	agency_raw := strings.ReplaceAll(split[0], "'", "")
+	number_raw := strings.ReplaceAll(split[1], "'", "")
+
+	agency, err := strconv.Atoi(agency_raw)
+	if err != nil {
+		return nil, err
+	}
+
+	number, err := strconv.Atoi(number_raw)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BetPacket{
+		Agency: agency,
+		Number: number,
+	}, nil
+}
+
+// Serialize Serializes a BetPacket into a byte array
+func (p *BetPacket) Serialize() []byte {
+	header := "bet "
+
+	msg := header +
+		"'" + fmt.Sprint(p.Agency) + "' " +
+		"'" + p.FirstName + "' " +
+		"'" + p.LastName + "' " +
+		"'" + p.Document + "' " +
+		"'" + p.Birthdate + "' " +
+		"'" + fmt.Sprint(p.Number) + "'\n"
+
+	return []byte(msg)
+}
